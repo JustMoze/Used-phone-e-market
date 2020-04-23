2ee31b1357c3374f95b834bed7ce34884be47e46
JustMoze <modestasrimeikis@gmail.com>
2020-04-23 14:08:59 +0300

    add user table and fix user validation back-end

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const config = require('config');
 const router = require('./routes/index');
+const userRoute = require('./routes/users');
 const mongoose = require('mongoose');
 const app = express();
 
 // Routes
+app.use(express.json());
 app.use('/', router);
+app.use('/api/users', userRoute);
 
-const PORT = process.env.PORT || config.get("port");
+const PORT = process.env.PORT || config.get('port');
 // connect to db
+const db = config.get('db');
 
-mongoose.connect(process.env.DB,  { useNewUrlParser: true } ,  () => {
-    console.log('Successfully connected to database');
-});
+// process.env.DB
+mongoose.set('useCreateIndex', true);
+mongoose.connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Successfully connected to database');
+    }
+);
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,12 +1,53 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const schema = new Schema({
-    name: {type: String, minlength: 5, maxlength: 25, required: true},
-    hash: {type: String, required: true},
-    email: {type: String, validate: {validator: () => Promise.resolve(false), message: 'Email validation failed'}},
-    createdDate: {type: Date, default: Date.now}
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
 });
 
-schema.set('toJSON', {virtuals: true});
-module.exports = mongoose.model('User', schema);
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            email: this.email
+        },
+        config.get('jwtPrivateKey')
+    );
+    return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(2).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
